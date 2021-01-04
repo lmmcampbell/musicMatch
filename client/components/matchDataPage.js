e9368dea80c0b3c400b8cca7b75009b91bb01c52
Laura <lmmcampbell@gmail.com>
2021-01-04 13:45:26 -0600

    cleaned up mobile view on match data page

--- client/components/matchDataPage.js
+++ client/components/matchDataPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchMatchSongs} from '../store/matchSongs'
 import {fetchMatchArtists} from '../store/matchArtists'
+import {Container, Row, Col} from 'react-bootstrap'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 export class MatchDataPage extends React.Component {
@@ -25,41 +26,53 @@ export class MatchDataPage extends React.Component {
     const matchSongs = this.props.matchSongs
     const matchArtists = this.props.matchArtists
     return (
-      <div style={{marginBottom: '10px'}}>
+      <Container fluid>
         <ListGroup className="top-list">
-          <h3 className="title">Shared Songs</h3>
-          <div>
-            {matchSongs && matchSongs.length ? (
-              matchSongs.map(song => {
-                return (
-                  <ListGroup.Item key={song.id} id={song.spotifyId}>
-                    <span className="italics">{song.name} </span>by{' '}
-                    {song.artists}
-                  </ListGroup.Item>
-                )
-              })
-            ) : (
-              <div>No shared songs!</div>
-            )}
-          </div>
+          <Row>
+            <Col>
+              <h3 className="title">Shared Songs</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={{span: 8, offset: 2}}>
+              {matchSongs && matchSongs.length ? (
+                matchSongs.map(song => {
+                  return (
+                    <ListGroup.Item key={song.id} id={song.spotifyId}>
+                      <span className="italics">{song.name} </span>by{' '}
+                      {song.artists}
+                    </ListGroup.Item>
+                  )
+                })
+              ) : (
+                <div>No shared songs!</div>
+              )}
+            </Col>
+          </Row>
         </ListGroup>
         <ListGroup className="top-list">
-          <h3 className="title">Shared Artists</h3>
-          <div>
-            {matchArtists && matchArtists.length ? (
-              matchArtists.map(artist => {
-                return (
-                  <ListGroup.Item key={artist.id} id={artist.spotifyId}>
-                    {artist.name}
-                  </ListGroup.Item>
-                )
-              })
-            ) : (
-              <div>No shared artists!</div>
-            )}
-          </div>
+          <Row>
+            <Col>
+              <h3 className="title">Shared Artists</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={{span: 8, offset: 2}}>
+              {matchArtists && matchArtists.length ? (
+                matchArtists.map(artist => {
+                  return (
+                    <ListGroup.Item key={artist.id} id={artist.spotifyId}>
+                      {artist.name}
+                    </ListGroup.Item>
+                  )
+                })
+              ) : (
+                <div>No shared artists!</div>
+              )}
+            </Col>
+          </Row>
         </ListGroup>
-      </div>
+      </Container>
     )
   }
 }
